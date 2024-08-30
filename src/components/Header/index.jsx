830d250eec2b1804e432d8fc5ecd1133eafafa2d
AnaCPuhl <aninha.silva2006@gmail.com>
2024-08-29 23:20:08 -0300

    Alteracoes28-08

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css'
+
+export default function Header(){
+    return (
+        <header id="cabecalho">
+            <h1>Minha página HTML</h1>
+            <a class="btn" href="index.html">Voltar</a>
+        </header>
+    )
+}
